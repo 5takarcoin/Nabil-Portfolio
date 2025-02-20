bc3b42654ea6e654bd20e4b08bcbe1d87ce51d7a
5takarcoin <5takarcoin@gmail.com>
2025-02-20 11:57:41 +0600

    Works added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,10 @@
 import works from "../assets/works.svg";
 import { OneProject } from "./builders/OneProject";
 
+import cool from "../assets/cool.png";
+import cool2 from "../assets/cool2.jpeg";
+import cool3 from "../assets/cool3.jpeg";
+
 export const About = () => {
   return (
     <div>
@@ -15,9 +19,9 @@ export const About = () => {
         ))}
       </div>
       <div className="h-90 flex gap-4 px-4 z-10">
-        <OneProject />
-        <OneProject />
-        <OneProject />
+        <OneProject cool={cool} />
+        <OneProject cool={cool2} />
+        <OneProject cool={cool3} />
       </div>
     </div>
   );
--- src/components/builders/OneProject.jsx
+++ src/components/builders/OneProject.jsx
@@ -1,3 +1,27 @@
-export const OneProject = () => {
-  return <div className="w-full h-full rounded-lg bg-red-900">Ahaa</div>;
+import { useState } from "react";
+import { IoMdEye } from "react-icons/io";
+
+export const OneProject = ({ cool, offset = "" }) => {
+  const [hovering, setHovering] = useState(false);
+
+  return (
+    <div
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
+      className="flex relative items-start justify-center w-full h-full rounded-lg overflow-hidden"
+    >
+      <img
+        src={cool}
+        className={`${
+          hovering && "scale-125 " + offset
+        } transition-transform duration-300`}
+        alt=""
+      />
+      {hovering && (
+        <div className="absolute flex items-center justify-center bg-black/60 h-full w-full">
+          <IoMdEye className="text-white text-4xl" />
+        </div>
+      )}
+    </div>
+  );
 };
