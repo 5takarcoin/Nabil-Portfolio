c1083ae915b947329a2f726495e8285c80fff7fe
5takarcoin <5takarcoin@gmail.com>
2025-02-20 13:14:15 +0600

    Medium screen done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { FaArrowDown } from "react-icons/fa";
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
-import { useState } from "react";
 import { Cover } from "./components/Cover";
 
 function App() {
-  const [cover, setCover] = useState(true);
-
   return (
     <>
       <Navbar />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,18 @@
+import thank from "../assets/thank.svg";
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="h-24 -mt-8 flex gap-12 items-end overflow-hidden">
+        {Array.from({ length: 24 }).map((_, index) => (
+          <img
+            key={index}
+            className="h-12 scale-90 -mb-1 -ml-5 -z-10  opacity-30"
+            src={thank}
+            alt=""
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
